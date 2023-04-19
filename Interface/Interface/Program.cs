62da2cef7787c7618930df2a6e5140c582521c9e
Sipho Matseme <smatseme@gmail.com>
2023-04-19 21:55:38 +0200

    Init Commit

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+
+    interface IVehicle {
+
+        void changeGear(int x);
+        void speedUp(int x);
+        void applyBrakes(int x);
+    }
+
+
+    class Bicycle : IVehicle {
+
+        int speed;
+        int gear;
+
+        public void applyBrakes(int decreament)
+        {
+            speed -= decreament;
+        }
+
+        public void changeGear(int nextGear)
+        {
+            gear = nextGear;
+        }
+
+        public void speedUp(int increament)
+        {
+            speed += increament;
+        }
+
+        public void printStates()
+        {
+            Console.WriteLine("speed: " + speed +
+                              " gear: " + gear);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bicycle bicycle = new Bicycle();
+            bicycle.changeGear(2);
+            bicycle.speedUp(4);
+            bicycle.applyBrakes(3);
+
+            Console.WriteLine("Bicycle present state");
+            bicycle.printStates();
+
+        }
+    }
+}
